70305132478be883bd026670a846a8229d76eb37
jarenadams21 <jarenadams3@gmail.com>
2023-03-22 17:27:35 -0400

    cleanup segmentation faults in opt 1

--- tutorials/OnlineBanking.c
+++ tutorials/OnlineBanking.c
@@ -17,8 +17,14 @@ int main() {
 
     // User for application
     struct user usr;
+
+    // Store users in a file
+    FILE *fp;
+    char filename[50];
+
     // User option selected
     int opt;
+
     printf("\nWhat do you want to do?");
     printf("\n\n1. Register an account");
     printf("\n2. Login to an account");
@@ -30,13 +36,24 @@ int main() {
         // Clears the screen
         system("clear");
 
-        printf("Enter your account number:\t");
-        scanf("%s", &usr.ac);
-        printf("\nEnter your phone number:\t");
-        scanf("s", &usr.phone);
+        printf("Enter your account #:\t");
+        scanf("%s", usr.ac);
+        printf("Enter your phone #:\t");
+        scanf("%s", usr.phone);
         printf("Enter your new password: \t");
         scanf("%s", usr.password);
         usr.balance = 0;
+
+        // User file storing
+        strcpy(filename, usr.phone);
+        fp = fopen(strcat(filename, ".dat"), "w");
+        fwrite(&usr, sizeof(struct user), 1, fp);
+
+        if(fwrite != 0) {
+            printf("\n\nAccount successfully registered!");
+        } else {
+            printf("\n\nSomething went wrong... please try again.");
+        }
     }
 
     return 0;
